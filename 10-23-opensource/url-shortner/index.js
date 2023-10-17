3bb101e359980b944e36e55083c91f1ab0735f4a
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2023-10-17 19:37:23 +0000

    update index.js to return redirect url

--- 10-23-opensource/url-shortner/index.js
+++ 10-23-opensource/url-shortner/index.js
@@ -1,2 +1,8 @@
 //load url from redirect.yml
+const YAML = require('yaml')
+const fs = require('fs')
+const path = require('path')
+
+const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirect.yml'), 'utf-8')
+console.log(redirectsFile)
 //geneurl page for each url from template.html
