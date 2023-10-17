bbc4e76fa75a1ef96d0d4aa04e54a2e138965e3d
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2023-10-17 19:50:14 +0000

    Loop through redirects

--- 10-23-opensource/url-shortner/index.js
+++ 10-23-opensource/url-shortner/index.js
@@ -6,7 +6,15 @@ const path = require('path')
 const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirect.yml'), 'utf-8')
 //console.log(redirectsFile)
 const redirects = YAML.parse(redirectsFile)
-console.log(redirects)
-
+//console.log(redirects)
 
 //geneurl page for each url from template.html
+
+const templateHML = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8')
+
+//move through all url redirects and genere an html page
+
+for (let [slug, url] of Object.entries(redirects))
+{
+    console.log("Generating HTML page for", slug)
+}
