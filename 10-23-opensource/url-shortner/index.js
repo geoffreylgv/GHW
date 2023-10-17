669d8647a5610d2802db380b1afd81ec3b04b58d
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2023-10-17 19:57:30 +0000

    generate folders

--- 10-23-opensource/url-shortner/index.js
+++ 10-23-opensource/url-shortner/index.js
@@ -19,4 +19,8 @@ for (let [slug, url] of Object.entries(redirects))
     console.log("Generating HTML page for", slug)
 
     const html = templateHML.replaceAll("https://example.com", url)
+
+    //create folder for each slug
+    const folderPath = path.join(__dirname, 'out', slug)
+    fs.mkdirSync(folderPath, { recursive: true })
 }
