7c4456f458cd9498db7041ed0a444a34e25f1d3b
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2023-10-17 19:42:44 +0000

    update index js and integrate the yaml part to have redirect as object

--- 10-23-opensource/url-shortner/index.js
+++ 10-23-opensource/url-shortner/index.js
@@ -4,5 +4,9 @@ const fs = require('fs')
 const path = require('path')
 
 const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirect.yml'), 'utf-8')
-console.log(redirectsFile)
+//console.log(redirectsFile)
+const redirects = YAML.parse(redirectsFile)
+console.log(redirects)
+
+
 //geneurl page for each url from template.html
